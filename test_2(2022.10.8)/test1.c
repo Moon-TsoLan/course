2d84bdb8826b76fecb0cbc8488ae16bc5ba7682a
Moon-TsoLan <15529518503@163.com>
2022-12-13 23:30:10 +0800

    第五次测试

---
+++ test_2(2022.10.8)/test1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	
+	printf("%d\n", 100);
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	return 0;
+}
+
+
+//打印一个整数-%d  
+//1个字节（byte）等于8个比特位（bit） 1kb=1024byte 1mb=1024kb 1gb=1024mb 1tb=1024gb 1pb=1024tb
