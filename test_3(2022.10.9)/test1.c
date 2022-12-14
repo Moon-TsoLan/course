cbb54db1610f2337011ac5cc5b19f946401531d0
Moon-TsoLan <15529518503@163.com>
2022-12-14 22:40:12 +0800

    第六次测试

---
+++ test_3(2022.10.9)/test1.c
@@ -0,0 +1,52 @@
+//#include<stdio.h>
+//int main()
+//{
+//	printf("心情复杂\n");
+//
+//
+//	return 0;
+//}
+
+//
+//
+//
+//
+//#include<stdio.h>
+//int main()
+//{//int指类型
+//	int age = 20;
+//	double weight = 70.1;
+//	age = age + 1;
+//	weight = weight - 10;
+//	printf("%d\n", age);
+//	printf("%lf\n", weight);
+//
+//	return 0;
+//}
+//
+
+//
+//#include<stdio.h>
+////大括号外为外部定义的，是全局变量
+//int a = 100;
+//int main()
+//{
+//	//大括号内为内部定义的，是内部变量
+//	//当局部变量和全局变量名字冲突时，局部优先
+//	int a = 10;
+//	printf("%d\n", a);
+//
+//	return 0;
+// 
+// 
+//}
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+
+	return 0;
+}
